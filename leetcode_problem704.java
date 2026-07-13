f7e4bce468c3ecdaad119e039ef91f478cdcd16d
Rajesh-8124 <rajesh100998kumar@gmail.com>
2026-07-13 16:52:35 +0530

    problem704

---
+++ leetcode_problem704.java
@@ -0,0 +1,24 @@
+public class leetcode_problem704{
+  public static void main(String[] args){
+    int[] nums = {-1, 0, 3, 5, 9, 12};
+    int target = 9;
+    System.out.println(binarysearch(nums, target));
+  }
+  public static int binarysearch(int[] nums, int target){
+    int left = 0;
+    int right = nums.length - 1;
+    while(left <= right){
+      int mid = left + (right - left)/2;
+      if(nums[mid] == target){
+        return mid;
+      }
+      else if(nums[mid] < target){
+        left = mid + 1;
+      }
+      else{
+        right = mid - 1;
+      }
+    }
+    return -1;
+  }
+}
